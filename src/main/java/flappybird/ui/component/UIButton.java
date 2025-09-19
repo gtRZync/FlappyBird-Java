3df158d267d04dada08e5e7301e973ebbe140ef5
Myson Dio <sendorfbg@gmail.com>
2025-09-19 00:03:37 -0300

    feat: completing ImageTextRender class for better score rendering

--- src/main/java/flappybird/ui/component/UIButton.java
+++ src/main/java/flappybird/ui/component/UIButton.java
@@ -1,22 +1,24 @@
 package flappybird.ui.component;
 
+import flappybird.graphics.texture.Texture;
+
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 
 public class UIButton extends UIElement{
     private String text;
-    private BufferedImage icon;
+    private Texture icon;
 
-    public UIButton(int x, int y, int width, int height, BufferedImage icon) {
+    public UIButton(int x, int y, int width, int height, Texture icon) {
         super(x, y, width, height);
         this.icon = icon;
     }
 
     private void renderImage(Graphics2D g2) {
-        g2.drawImage(icon, x, y , width, height, null);
+        g2.drawImage(icon.getImage(), x, y , width, height, null);
     }
 
-    public void setIcon(BufferedImage icon) {
+    public void setIcon(Texture icon) {
         this.icon = icon;
     }
 
--- src/main/java/flappybird/ui/imagetext/ImageTextRenderer.java
+++ src/main/java/flappybird/ui/imagetext/ImageTextRenderer.java
@@ -1,11 +1,63 @@
 package flappybird.ui.imagetext;
 
+import flappybird.graphics.sprite.Sprite;
+import flappybird.graphics.sprite.SpriteManager;
+import flappybird.math.Vector2;
+
+import java.awt.Graphics2D;
+import java.util.List;
+import java.util.ArrayList;
+
+//!Might delete it if this its only use
 public class ImageTextRenderer {
-//    class ImageTextRenderer:
-//         def __init__(self, image_map):
-//         self.image_map = image_map  # e.g., {'A': 'A.png', '1': '1.png'}
-//
-//         def draw_text(self, text, position):
-//                 # logic to draw the text using the images
-//         pass
+    private List<Sprite> scoreSprite;
+    private int prevScore = -1;
+    private final Vector2<Integer> windowSize;
+    private final int defaultWidth;
+
+    public ImageTextRenderer(Vector2<Integer> windowSize) {
+        this.windowSize = windowSize;
+        Sprite zero = new Sprite(SpriteManager.getTexture("0"));
+        scoreSprite = new ArrayList<>();
+        scoreSprite.add(zero);
+        this.defaultWidth = scoreSprite.getFirst().getWidth();
+    }
+
+    private List<Sprite> scoreToSprites(int score) {
+        char[] arr = Integer.toString(score).toCharArray();
+        List<Sprite> sprites = new ArrayList<>();
+        for(char c : arr) {
+           Sprite sprite = new Sprite(SpriteManager.getTexture(Character.toString(c)));
+           sprites.add(sprite);
+        }
+        return sprites;
+    }
+
+    public void renderScore(Graphics2D g2, int score) {
+        boolean scoreUpdated = (prevScore != score);
+        if(scoreUpdated) {
+            scoreSprite = scoreToSprites(score);
+            int paddingX = 6;
+            int sizeW = 0;
+            int sizeH = scoreSprite.getFirst().getHeight(); //?every number have the same height
+            int w = windowSize.x;
+            int h = windowSize.y;
+            for(Sprite s : scoreSprite) {
+                sizeW += defaultWidth;
+                if(s != scoreSprite.getLast()) {
+                    sizeW += paddingX;
+                }
+            }
+            int posX = (w - sizeW) / 2;
+            int posY = (h - sizeH) / 10;
+            for(int i = 0 ; i < scoreSprite.size(); i++) {
+                scoreSprite.get(i).setPosition(posX + (i * (defaultWidth + paddingX )), posY);
+            }
+            prevScore = score;
+        }
+        for(Sprite s : scoreSprite)
+        {
+            s.render(g2);
+        }
+    }
 }
