10f50fb7a884be8c2827e243c44ff8c277c46502
Myson Dio <sendorfbg@gmail.com>
2025-09-24 17:12:44 -0300

    removing sprites constant, since i deleted the 40 other single sprite per png

--- src/main/java/flappybird/core/GameConstants.java
+++ src/main/java/flappybird/core/GameConstants.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 public class GameConstants {
     public static final int DAYTIME_DURATION = 15; //!seconds
     public static final String GAME_NAME = "FlappyBird Clone";
-    public static final String SPRITES_DIR = "/assets/sprites/";
     public static final String JSON_FILE_PATH = "/assets/sprites/sprite_sheet.json";
     public static final String SPRITESHEET_PATH = "/assets/sprites/sprite_sheet.png";
     public static final String AUDIO_DIR = "/assets/audio/";
@@ -24,29 +23,4 @@ public class GameConstants {
     public static final float X_SPEED = -2.f;
     public static final int SPACE_BETWEEN_PIPES = 180;
     public static final float MAX_HEIGHT = -100.f;
-
-    public static BufferedImage BACKGROUND;
-    public static BufferedImage FLOOR;
-    public static BufferedImage[] BIRD = new BufferedImage[3];
-    public static BufferedImage PIPE;
-    public static BufferedImage MESSAGE;
-    public static BufferedImage TITLE;
-
-
-    static
-    {
-        try
-        {
-            MESSAGE = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"message.png")));
-            TITLE = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"FlappyBird.png")));
-            BACKGROUND = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"background-day.png")));
-            PIPE = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"pipe2.png")));
-            BIRD[0] = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"yellowbird-upflap.png")));
-            BIRD[1] = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"yellowbird-midflap.png")));
-            BIRD[2] = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"yellowbird-downflap.png")));
-            FLOOR = ImageIO.read(Objects.requireNonNull(GameConstants.class.getResource(SPRITES_DIR+"floor.png")));
-        }catch(IOException e) {
-            throw new RuntimeException("Failed to load game assets", e);
-        }
-    }
 }
